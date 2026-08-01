91198bb73c187b27b75775f9cd6818c616e5bd0e
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-01 12:59:54 +0200

    build: follow nixpkgs master for Dioxus CLI

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Plinth - personal website with Dioxus SSR";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
 
     crane.url = "github:ipetkov/crane";
 
