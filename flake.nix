b7b12c998a89a08dc8e6f9dd8eb8bb46f7d8f985
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-14 18:37:51 +0200

    fix: use build packages for generated metadata

--- flake.nix
+++ flake.nix
@@ -212,7 +212,7 @@
           inherit pkgs system;
           enableOsxcross = false;
         };
-        postgresqlWithPgvector = pkgs.postgresql_17.withPackages (ps: [ps.pgvector]);
+        postgresqlWithPgvector = pkgs.buildPackages.postgresql_17.withPackages (ps: [ps.pgvector]);
 
         # Parameterized build function for configurability
         buildPlinth = {
@@ -822,7 +822,7 @@
         projectReferencesJson = pkgs.writeText "plinth-project-references.json" (builtins.toJSON projectReferences);
         # The Nix-facing registry is keyed by project id, while the CLI
         # contract consumes a JSON array of manifest objects.
-        portfolioManifestsJson = pkgs.writeText "plinth-portfolio-manifests.json" (builtins.toJSON (lib.attrValues portfolioManifests));
+        portfolioManifestsJson = pkgs.buildPackages.writeText "plinth-portfolio-manifests.json" (builtins.toJSON (lib.attrValues portfolioManifests));
         portfolioManifestFiles = lib.mapAttrs (_: projectSiteLib.portfolioManifestFileFromDefinition) plinthProjects;
         site = projectSiteLib.mkProjectSiteFromDefinition plinthProjects.plinth;
         website = site;
